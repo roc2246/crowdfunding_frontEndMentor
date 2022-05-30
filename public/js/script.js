9ca9d644352b72349bd78f371908dc9d9a1b890c
Riley <riley.childs@yahoo.com>
2022-05-30 11:34:28 -0400

    Finished coding UI for odal options

--- public/js/script.js
+++ public/js/script.js
@@ -2,10 +2,22 @@ const modal = document.querySelector(".modal");
 const page = document.querySelector("body, .project-top");
 
 const toggleModal = (display1, display2) => {
-if(modal.style.display = display1){
-    modal.style.display = display2
-}
+  if ((modal.style.display = display1)) {
+    modal.style.display = display2;
+  }
 
-// page.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
+  // page.style.backgroundColor = "rgba(0,0,0,0.4)";
+};
 
+const option = document.getElementsByClassName("option");
+const radio = document.getElementsByClassName("radio");
+Object.keys(radio).forEach((rad) => {
+  radio[rad].addEventListener("change", () => {
+    if (radio[rad].checked === true) {
+      Object.keys(radio).forEach(
+        (rad) => (option[rad].style.borderColor = "#f4f4f4")
+      );
+      option[rad].style.borderColor = "hsl(176, 72%, 28%)";
+    }
+  });
+});
