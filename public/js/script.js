c47a06e5ae2ca27d417f5cd175e8d3898b40ce7f
Riley <riley.childs@yahoo.com>
2022-05-30 12:06:01 -0400

    Coded UX for pledge options

--- public/js/script.js
+++ public/js/script.js
@@ -10,14 +10,23 @@ const toggleModal = (display1, display2) => {
 };
 
 const option = document.getElementsByClassName("option");
+const divider = document.querySelectorAll(".option > hr");
+const pledge = document.getElementsByClassName("option__pledge");
 const radio = document.getElementsByClassName("radio");
 Object.keys(radio).forEach((rad) => {
   radio[rad].addEventListener("change", () => {
-    if (radio[rad].checked === true) {
-      Object.keys(radio).forEach(
-        (rad) => (option[rad].style.borderColor = "#f4f4f4")
-      );
+      Object.keys(radio).forEach((rad) => {
+        option[rad].style.borderColor = "#f4f4f4";
+        if(rad.length >= rad > 0 ) {
+            pledge[rad].style.display = "none";
+            divider[rad].style.display = "none";
+          } 
+      });
       option[rad].style.borderColor = "hsl(176, 72%, 28%)";
-    }
+      if(rad > 0) {
+        pledge[rad -1].style.display = "block";
+        divider[rad -1].style.display = "block";
+      } 
   });
 });
+
